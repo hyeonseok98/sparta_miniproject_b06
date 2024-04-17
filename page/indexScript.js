76880a79c74a2d19ab0a2977891aee80dadf4cae
sobbingtofu <kukubasak@naver.com>
2024-04-17 12:18:02 +0900

    update: 팀원 소개 페이지 1차 완성해 추가

--- page/indexScript.js
+++ page/indexScript.js
@@ -7,23 +7,134 @@ const DEFAULT_PAGENUMBER = 3;
 const LAST_PAGENUMBER = 2;
 let page = 0;
 
-window.addEventListener(
-  "wheel",
-  function (e) {
-    e.preventDefault();
-    if (e.deltaY > 0) {
-      page++;
-    } else if (e.deltaY < 0) {
-      page--;
-    }
-    if (page < 0) {
-      page = 0;
-    } else if (page > LAST_PAGENUMBER) {
-      page = LAST_PAGENUMBER;
-    }
-    console.log(e.deltaY);
-    wrap.style.transition = "top 0.5s ease";
-    wrap.style.top = page * -100 + "vh";
-  },
-  { passive: false },
-);
+const scrollSystem = (e) => {
+  e.preventDefault();
+  if (e.deltaY > 0) {
+    page++;
+  } else if (e.deltaY < 0) {
+    page--;
+  }
+  if (page < 0) {
+    page = 0;
+  } else if (page > LAST_PAGENUMBER) {
+    page = LAST_PAGENUMBER;
+  }
+  console.log(e.deltaY);
+  wrap.style.transition = "top 0.5s ease";
+  wrap.style.top = page * -100 + "vh";
+};
+
+window.addEventListener("wheel", scrollSystem, { passive: false });
+
+/**
+ * 윤기준 수정사항 => 스크롤 기능 구현하는 기존 함수를 분리해내 scrollSystem이라는 새로운 함수로 명명했습니다.
+ * 모달 열었을 때 해당 함수가 remove되고, 모달을 다시 닫았을 때 해당 함수를 다시 추가해
+ * 모달 열려있을 때 외부 스크롤이 작동하는걸 방지하기 위함입니다.
+ */
+
+/**
+ * 윤기준 JS
+ */
+const [TEAMMATE01, TEAMMATE02, TEAMMATE03, TEAMMATE04, TEAMMATE05, TEAMMATE06] = [
+  document.getElementById("teammate01"),
+  document.getElementById("teammate02"),
+  document.getElementById("teammate03"),
+  document.getElementById("teammate04"),
+  document.getElementById("teammate05"),
+  document.getElementById("teammate06"),
+];
+
+const [TEAMMATE01_MODAL, TEAMMATE02_MODAL, TEAMMATE03_MODAL, TEAMMATE04_MODAL, TEAMMATE05_MODAL, TEAMMATE06_MODAL] = [
+  document.getElementById("teammateModal01"),
+  document.getElementById("teammateModal02"),
+  document.getElementById("teammateModal03"),
+  document.getElementById("teammateModal04"),
+  document.getElementById("teammateModal05"),
+  document.getElementById("teammateModal06"),
+];
+
+const [
+  TEAMMATE01_MODAL_EXIT_BTN,
+  TEAMMATE02_MODAL_EXIT_BTN,
+  TEAMMATE03_MODAL_EXIT_BTN,
+  TEAMMATE04_MODAL_EXIT_BTN,
+  TEAMMATE05_MODAL_EXIT_BTN,
+  TEAMMATE06_MODAL_EXIT_BTN,
+] = [
+  document.getElementById("modalExitBtn01"),
+  document.getElementById("modalExitBtn02"),
+  document.getElementById("modalExitBtn03"),
+  document.getElementById("modalExitBtn04"),
+  document.getElementById("modalExitBtn05"),
+  document.getElementById("modalExitBtn06"),
+];
+
+const showProfile01 = () => {
+  TEAMMATE01_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+const showProfile02 = () => {
+  TEAMMATE02_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+const showProfile03 = () => {
+  TEAMMATE03_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+const showProfile04 = () => {
+  TEAMMATE04_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+const showProfile05 = () => {
+  TEAMMATE05_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+const showProfile06 = () => {
+  TEAMMATE06_MODAL.style.display = "block";
+  window.removeEventListener("wheel", scrollSystem);
+};
+
+const exitModal01 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+const exitModal02 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+const exitModal03 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+const exitModal04 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+const exitModal05 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+const exitModal06 = (event) => {
+  let exitTargetModal = event.target.parentElement.parentElement;
+  exitTargetModal.style.display = "none";
+  window.addEventListener("wheel", scrollSystem, { passive: false });
+};
+
+TEAMMATE01.addEventListener("click", showProfile01);
+TEAMMATE02.addEventListener("click", showProfile02);
+TEAMMATE03.addEventListener("click", showProfile03);
+TEAMMATE04.addEventListener("click", showProfile04);
+TEAMMATE05.addEventListener("click", showProfile05);
+TEAMMATE06.addEventListener("click", showProfile06);
+
+TEAMMATE01_MODAL_EXIT_BTN.addEventListener("click", exitModal01);
+TEAMMATE02_MODAL_EXIT_BTN.addEventListener("click", exitModal02);
+TEAMMATE03_MODAL_EXIT_BTN.addEventListener("click", exitModal03);
+TEAMMATE04_MODAL_EXIT_BTN.addEventListener("click", exitModal04);
+TEAMMATE05_MODAL_EXIT_BTN.addEventListener("click", exitModal05);
+TEAMMATE06_MODAL_EXIT_BTN.addEventListener("click", exitModal06);
